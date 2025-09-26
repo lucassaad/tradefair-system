6d94cefbc39392fa4d40ee6e3d21f83b356c7810
Lucas Saad <lucassaadro@gmail.com>
2025-09-26 17:51:36 -0300

    Initial commit

---
+++ tests/test_app.py
@@ -0,0 +1,16 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_zero.app import app
+
+client = TestClient(app)
+
+
+def test_root():
+    client = TestClient(app)
+
+    response = client.get('/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {"project": "tradefair-system"}
